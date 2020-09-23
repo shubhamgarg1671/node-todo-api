2d343a3d45e447b45ee1079edf4943a480f9b7a0
unknown <shubhamgarg1671@gmail.com>
2020-09-23 07:07:18 +0530

    Add post users/login/route

--- playground/hashing.js
+++ playground/hashing.js
@@ -7,37 +7,37 @@ var password = '123abc!';
 bcrypt.genSalt(10);
 
 
-// var data ={
-//     id: 10
-// }
+/*var data ={
+    id: 10
+}*/
 
-// var token = jwt.sign(data, '123abc');
-// console.log(token);
-// var decoded = jwt.verify(token, '123abc');
-// console.log('decoded', decoded);
+/*var token = jwt.sign(data, '123abc');
+console.log(token);
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
+*/
+/*var message = 'I am user number 3';
+var hash = SHA256(message).toString();
 
-// var message = 'I am user number 3';
-// var hash = SHA256(message).toString();
+console.log(`message ${message}`);
+console.log(`Hash ${hash}`);
 
-// console.log(`message ${message}`);
-// console.log(`Hash ${hash}`);
-
-// var data = {
-//     id: 4
-// }
-// var token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// };
-
-
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(data)).toString()
+var data = {
+    id: 4
+}
+var token = {
+    data,
+    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+};
 
 
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-// if (resultHash === token.hash) {
-//     console.log('Data was not changed');
-// } else {
-//     console.log('Data was changed');
-// }
+token.data.id = 5;
+token.hash = SHA256(JSON.stringify(data)).toString()
+
+
+var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+if (resultHash === token.hash) {
+    console.log('Data was not changed');
+} else {
+    console.log('Data was changed');
+}*/
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { Users } = require('./models/user');
 const { compact } = require('lodash');
-var {authenticate} = require('./middleware/authenticate');
+var { authenticate } = require('./middleware/authenticate');
 const port = process.env.PORT || 3000;
 var app = express();
 
@@ -104,11 +105,22 @@ app.post('/users', (req, res) => {
     })
 });
 
-
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 
+});
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    Users.findByCredentials(body.email, body.password).then((user) => {
+        // res.send(user); 
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
